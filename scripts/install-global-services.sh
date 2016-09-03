e50c33ba8d277c7a48f731183d80ed9e5743ba60
Ron Kurr <kurr@kurron.org>
2016-09-03 15:57:47 -0400

    Make Plex a global, constrained service

--- scripts/install-global-services.sh
+++ scripts/install-global-services.sh
@@ -16,6 +16,29 @@ docker service create --mode global \
 
 docker service inspect --pretty data-dog
 
+# required volumes:
+#     ./create-nfs-volume-read-only.sh 192.168.1.234 /media media
+#     ./create-nfs-volume-read-only.sh 192.168.1.234 /atlantis-sync torrent-sync
+#     ./create-nfs-volume-read-write.sh 192.168.1.234 /bifrost-pc plex-config
+
+echo Plex Media Server
+
+docker service create --mode global \
+                      --name plex \
+                      --constraint 'node.labels.plex-capable==true' \
+                      --publish 32400:32400 \
+                      --label org.kurron.purpose="fun" \
+                      --env SKIP_CHOWN_CONFIG="TRUE" \
+                      --log-driver json-file \
+                      --log-opt max-size=128m \
+                      --log-opt max-file=4 \
+                      --mount type=volume,src=plex-config,dst=/config \
+                      --mount type=volume,src=torrent-sync,dst=/mnt/torrent \
+                      --mount type=volume,src=media,dst=/mnt/media \
+                      timhaak/plex:latest
+
+docker service inspect --pretty plex
+
 docker service ls
 
 #docker service ps data-dog 
--- scripts/install-replicated-services.sh
+++ scripts/install-replicated-services.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# required volumes:
-#     ./create-nfs-volume-read-only.sh 192.168.1.234 /media media
-#     ./create-nfs-volume-read-only.sh 192.168.1.234 /atlantis-sync torrent-sync
-#     ./create-nfs-volume-read-write.sh 192.168.1.234 /bifrost-pc plex-config
-
-echo Plex Media Server
-
-docker service create --mode replicated \
-                      --replicas=1 \
-                      --name plex \
-                      --publish 32400:32400 \
-                      --label org.kurron.purpose="fun" \
-                      --env SKIP_CHOWN_CONFIG="TRUE" \
-                      --log-driver json-file \
-                      --log-opt max-size=128m \
-                      --log-opt max-file=4 \
-                      --mount type=volume,src=plex-config,dst=/config \
-                      --mount type=volume,src=torrent-sync,dst=/mnt/torrent \
-                      --mount type=volume,src=media,dst=/mnt/media \
-                      timhaak/plex:latest
-
-docker service inspect --pretty plex
-
 docker service ls
 
 #docker service ps plex
