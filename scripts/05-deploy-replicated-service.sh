bfdae2caf9cf6ca36451f8b00c4f534c85eae53e
Ron Kurr <kurr@kurron.org>
2017-04-14 12:11:45 -0400

    Debug the scale command

--- scripts/05-deploy-replicated-service.sh
+++ scripts/05-deploy-replicated-service.sh
@@ -24,7 +24,9 @@ sleep 1
 
 echo Scale the services up
 SERVICE_ID=$(docker stack ps ${STACK_NAME} --quiet)
-docker service scale ${SERVICE_ID}=16
+CMD="docker service scale ${SERVICE_ID}=16"
+echo $CMD
+${CMD}
 
 echo Listing all services in the stack
 watch docker stack services ${STACK_NAME}
