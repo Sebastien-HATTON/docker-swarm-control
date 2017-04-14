1a86c69f491c04c229fbaa3c0902f24ef473d00d
Ron Kurr <kurr@kurron.org>
2017-04-14 15:48:45 -0400

    Used the wrong descriptor which explains why it was not working!

--- scripts/07-deploy-rolling-upgrade.sh
+++ scripts/07-deploy-rolling-upgrade.sh
@@ -4,7 +4,7 @@
 # NOTE: must be run on the master node
 
 DESCRIPTOR_V1=`pwd`/descriptors/rolling-upgrade-v1-service-definition.yml
-DESCRIPTOR_V2=`pwd`/descriptors/rolling-upgrade-v1-service-definition.yml
+DESCRIPTOR_V2=`pwd`/descriptors/rolling-upgrade-v2-service-definition.yml
 STACK_NAME=upgrade-test
 
 echo Deploying Stack
@@ -28,17 +28,16 @@ clear
 
 echo Deploying Upgraded Stack
 docker stack deploy --compose-file ${DESCRIPTOR_V2} ${STACK_NAME}
-sleep 2
 clear
 
 echo List all stacks
 docker stack ls
-sleep 2
+sleep 4
 clear
 
 echo Listing all services in the stack
 docker stack services ${STACK_NAME}
-sleep 2
+sleep 4
 clear
 
 echo Listing all tasks in the stack
