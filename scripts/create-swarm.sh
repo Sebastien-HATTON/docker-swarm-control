321ba70fe634f2df1947c4b5284d9576476f2f8b
Ron Kurr <kurr@kurron.org>
2017-04-14 09:29:03 -0400

    Build out the swarm using the new ip addresses

--- scripts/create-swarm.sh
+++ scripts/create-swarm.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo Creating the Swarm, making this host the Manager
-#docker swarm init --advertise-addr 10.10.10.20
-docker swarm init
+docker swarm init --advertise-addr 10.10.10.10
 
 echo Capture the manager and worker tokens
 MANAGER_TOKEN=$(docker swarm join-token --quiet manager)
@@ -10,6 +9,12 @@ WORKER_TOKEN=$(docker swarm join-token --quiet worker)
 
 echo Manager Token is ${MANAGER_TOKEN}
 echo Worker Token is ${WORKER_TOKEN}
+
+echo Joining remaining nodes to the Swarm as Workers
+ssh 10.10.10.11 sudo docker swarm join --token $WORKER_TOKEN 10.10.10.10:2377
+ssh 10.10.10.12 sudo docker swarm join --token $WORKER_TOKEN 10.10.10.10:2377
+ssh 10.10.10.13 sudo docker swarm join --token $WORKER_TOKEN 10.10.10.10:2377
+ssh 10.10.10.14 sudo docker swarm join --token $WORKER_TOKEN 10.10.10.10:2377
 
 echo List nodes in the Swarm
 docker node ls
